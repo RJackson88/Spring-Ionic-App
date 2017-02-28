48b561429b33fe1fc4fa6eeb81ef21347559da4e
rjackson88 <richardwjacksonjr@yahoo.com>
2017-02-28 14:19:12 -0500

    Initial commit

---
+++ src/main/java/stg/SpringionicApplication.java
@@ -0,0 +1,12 @@
+package stg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringionicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringionicApplication.class, args);
+	}
+}
